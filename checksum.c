252fc870ba8752531029e6897b85c9dd5f9eedea
Brad Imhof <78041259+Bradx5@users.noreply.github.com>
2021-02-17 00:12:18 -0800

    Add files via upload

---
+++ checksum.c
@@ -0,0 +1,61 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <sys/types.h
+#include <unistd.h>
+
+#define max_int (255)
+#define byte unsigned char
+
+int main (int argc, char * argv[], char ** envp) {
+
+
+
+
+inc c;
+int count = 10;
+int sum = 0;
+ //int fd;
+  byte checksum; 
+  byte complement;
+    //fd = open(STDIN_FILENO, O_RDONLY);
+  
+ // int *p = (int *) &buffer;
+
+
+  byte header[10]
+
+read(STDIN_FILENO, &header, 10);
+
+ for (c = 0; c <= count ; c ++);
+ {
+ if (c == 5) 
+ {
+   checksum = header[c];
+   header[c] = 0;
+ }
+sum += header[c];
+{
+if (sum > max_int)
+sum = 0;
+ }}
+complement = max_int - sum;
+
+
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
+}
+//https://github.com/Bradx5/checksum.c
