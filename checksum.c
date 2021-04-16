c1761bde13465f8136f854e27cd8d31b878303f0
Brad Imhof <78041259+Bradx5@users.noreply.github.com>
2021-04-15 18:00:18 -0700

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -1,17 +1,6 @@
-/********************************/
-/* Program Name:                */
-/* Author:                      */
-/* Date:                        */
-/********************************/
-/* Description:                 */
-/* Validation Checks:           */
-/* Enhancements:                */
-/********************************/
-
-#include "stdio.h"
-#include "stdlib.h"
-#include <sys/types.h
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* for using read(..) */
 
 #define max_int (255)
 #define byte unsigned char
@@ -25,37 +14,47 @@ int c;
 int count = 10;
 int sum = 0;
  //int fd;
-  byte checksum; 
+  byte checksum;
   byte complement;
     //fd = open(STDIN_FILENO, O_RDONLY);
-  
+
  // int *p = (int *) &buffer;
 
+unsigned char header[10];
 
-  byte header[10];
 
-read(STDIN_FILENO, &header, 10);
 
- for (c = 0; c <= count ; c ++);
+//read(STDIN_FILENO, &header, 10);
+
+read(STDIN_FILENO, header, count);
+
+ for (c = 0; c < count ; c++)
  {
- if (c == 5) 
- {
+ if (c == 5)
    checksum = header[c];
-   header[c] = 0;
- }
+  else {
 sum += header[c];
-{
-if (sum > max_int)
-sum = 0;
- }}
-complement = max_int - sum;
+if (sum > max_int) {
+sum = (sum & 0xFF) + 1;
+}
+}
+}
+}
+
+complement = ~sum;
+
+
+
 
 
   fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
-  if (checksum != complement ) {
-    fprintf(stderr, "Error Detected!\n"); 
-    return 1;
-  }
-  return 0;
+if (checksum != complement ) {
+fprintf(stderr, "Error Detected!\n");
+return 1;
+}
+return 0;
 }
 //https://github.com/Bradx5/checksum.c
+
+
+
